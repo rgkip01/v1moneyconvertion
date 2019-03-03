19497d0867103bcad75b05fda591b635cf9fd9b2
Rafael Abreu <web.nogs@gmail.com>
2019-03-03 14:33:18 -0300

    create controller Exchenges and implement tdd to requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'exchanges/index'
+  get 'convert', to: 'eschanges#convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
