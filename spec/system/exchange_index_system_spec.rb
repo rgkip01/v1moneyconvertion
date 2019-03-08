470491e1d694f8a3411a53e9387e69264b026dbd
Rafael Abreu <web.nogs@gmail.com>
2019-03-07 22:10:43 -0300

    added templates for view

---
+++ spec/system/exchange_index_system_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Exchange Curency Process", :type => :system, js: true do
+  it "Show exchange value" do
+    visit "/"
+    within("#exchange_form") do
+      select("USD", from: 'source_currency')
+      select("BRL", from: 'target_currency')
+
+      fill_in "amount", with: '10'
+    end
+    click_button 'Converter'
+
+    #save_and_open_page
+    expect(page).to have_content(value)
+  end
+end
